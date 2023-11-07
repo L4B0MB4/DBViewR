77b65c111d2bb4ec1f4daf7bfce2f5e5046a8cc2
L4B0MB4 <metalfan65@gmail.com>
2023-11-07 08:24:44 +0100

    init

---
+++ Frontend/dbviewr/src/flow/Flow.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+  Connection,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  { id: "3", position: { x: 0, y: 200 }, data: { label: "3" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export const Flow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (conn: Connection) => {
+      console.log("hier");
+      console.log(conn);
+      return setEdges((eds) => addEdge(conn, eds));
+    },
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+};
